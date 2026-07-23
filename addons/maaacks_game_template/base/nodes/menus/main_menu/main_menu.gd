19bd3185a0aa7b5b9a00d0d0e44a0ba194379858
guntur <gunturpdng@gmail.com>
2026-07-23 16:08:15 +0800

    feat: coupled safe area margin into its own scene

--- addons/maaacks_game_template/base/nodes/menus/main_menu/main_menu.gd
+++ addons/maaacks_game_template/base/nodes/menus/main_menu/main_menu.gd
@@ -110,7 +110,6 @@ func _ready() -> void:
 	_hide_options_if_unset()
 	_hide_credits_if_unset()
 	_hide_new_game_if_unset()
-	_apply_safe_area()
 
 func _on_new_game_button_pressed() -> void:
 	new_game()
@@ -126,20 +125,3 @@ func _on_exit_button_pressed() -> void:
 
 func _on_exit_confirmation_confirmed():
 	exit_game()
-	
-func _apply_safe_area() -> void:
-	var screen_size := Vector2(DisplayServer.screen_get_size())
-	var safe_area := DisplayServer.get_display_safe_area()
-	var viewport_size := get_viewport().get_visible_rect().size
-	
-	var scale := viewport_size / screen_size
-
-	var left := safe_area.position.x * scale.x
-	var top := safe_area.position.y * scale.y
-	var right := (screen_size.x - safe_area.end.x) * scale.x
-	var bottom := (screen_size.y - safe_area.end.y) * scale.y
-
-	offset_left = left
-	offset_top = top
-	offset_right = right
-	offset_bottom = bottom
---
+++ scenes/utilities/safe_area_margin_container.gd
@@ -0,0 +1,22 @@
+extends Control
+
+func _ready() -> void:
+	_apply_safe_area()
+
+
+func _apply_safe_area() -> void:
+	var screen_size := Vector2(DisplayServer.screen_get_size())
+	var safe_area := DisplayServer.get_display_safe_area()
+	var viewport_size := get_viewport().get_visible_rect().size
+	
+	var scale := viewport_size / screen_size
+
+	var left := safe_area.position.x * scale.x
+	var top := safe_area.position.y * scale.y
+	var right := (screen_size.x - safe_area.end.x) * scale.x
+	var bottom := (screen_size.y - safe_area.end.y) * scale.y
+
+	offset_left = left
+	offset_top = top
+	offset_right = -right
+	offset_bottom = -bottom
