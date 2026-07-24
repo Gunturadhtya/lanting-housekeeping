af585583236717fa72c2aa1cbe728d49ae46751f
guntur <gunturpdng@gmail.com>
2026-07-24 15:46:50 +0800

    feat: make cone sensor to be rotateable

--- ecs/systems/cone_sensor_system.gd
+++ ecs/systems/cone_sensor_system.gd
@@ -9,6 +9,8 @@ func process(world : ECSWorld, _delta : float) -> void:
 		var sensor_xform : TransformComponent = world.get_component(sensor_id, TransformComponent)
 		var sensor_faction : FactionComponent = world.get_component(sensor_id, FactionComponent)
 		var combat : CombatComponent = world.get_component(sensor_id, CombatComponent)
+		var motion : MotionComponent = world.get_component(sensor_id, MotionComponent)
+		var is_stationary := motion == null or motion.velocity.is_equal_approx(Vector2.ZERO)
 		var forward := Vector2.RIGHT.rotated(sensor_xform.rotation)
 		var half_fov := sensor.fov_degrees * 0.5
 		var best_id := -1
@@ -29,5 +31,8 @@ func process(world : ECSWorld, _delta : float) -> void:
 			if distance < best_distance:
 				best_distance = distance
 				best_id = target_id
+		if best_id != -1 and is_stationary:
+			var target_xform : TransformComponent = world.get_component(best_id, TransformComponent)
+			sensor_xform.rotation = (target_xform.position - sensor_xform.position).angle()
 		if combat:
 			combat.target_id = best_id
